d6ce18922d418b160e979226c08a902fe2ee7ada
Kyle Nedbal <kylenedbal@quickenloans.com>
2021-05-13 14:09:50 -0400

    using mvc to create a sign in page

---
+++ Lab11.2CoffeeShopRegistration/Controllers/ResultController.cs
@@ -0,0 +1,24 @@
+﻿using Lab11._2CoffeeShopRegistration.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab11._2CoffeeShopRegistration.Controllers
+{
+    public class ResultController : Controller
+    {
+        [HttpPost]
+        public IActionResult Index(User user)
+        {
+            Models.User.currentUser = user;
+            return View(user);
+        }
+
+        public IActionResult Profile()
+        {
+            return View(Models.User.currentUser);
+        }
+    }
+}
---
+++ Lab11.2CoffeeShopRegistration/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab11._2CoffeeShopRegistration.Models
+{
+    public class User
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string phoneNumber { get; set; }
+        public string password { get; set; }
+        public string confirmPassword { get; set; }
+        public int age { get; set; }
+
+        public static User currentUser;
+
+    }
+}
